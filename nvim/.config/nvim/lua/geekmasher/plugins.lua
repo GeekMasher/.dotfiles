7b81b513e7faeab65b547ca01098a848edbef578
GeekMasher <geekmasher@gmail.com>
2022-09-28 20:09:43 +0100

    More to alt Packer config

--- nvim/.config/nvim/lua/geekmasher/plugins.lua
+++ nvim/.config/nvim/lua/geekmasher/plugins.lua
@@ -5,53 +5,53 @@ if (not status) then
 end
 
 -- Packer
-packer.startup(function()
+vim.cmd [[packadd packer.nvim ]]
+packer.startup(function(use)
 
     -- Keeps Packer updating Packer
-    use('wbthomason/packer.nvim')
+    use 'wbthomason/packer.nvim'
      
     -- Utils
-    use('nvim-lua/plenary.nvim')
-    use('nvim-lua/popup.nvim')
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/popup.nvim'
 
     -- Themes
-    use('gruvbox-community/gruvbox')
-    use('flazz/vim-colorschemes')
-    use('folke/tokyonight.nvim')
-    use('kyazdani42/nvim-web-devicons')
+    use 'gruvbox-community/gruvbox'
+    use 'flazz/vim-colorschemes'
+    use 'folke/tokyonight.nvim'
+    use 'kyazdani42/nvim-web-devicons'
 
     -- Telescope
-    use('nvim-telescope/telescope.nvim')
+    use 'nvim-telescope/telescope.nvim'
 
     -- Treesitter
-    use("nvim-treesitter/nvim-treesitter", {
-        run = ":TSUpdate"
-    })
-    use("nvim-treesitter/playground")
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+    use 'nvim-treesitter/playground'
 
 
     -- LSP Magic
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/nvim-compe")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("onsails/lspkind-nvim") 
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/nvim-compe"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "onsails/lspkind-nvim"
 
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-buffer")
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-buffer"
 
     -- Snippets
-    use("hrsh7th/cmp-vsnip")
-    use("hrsh7th/vim-vsnip")
+    use "hrsh7th/cmp-vsnip"
+    use "hrsh7th/vim-vsnip"
 
     -- LSP saga
-    use("glepnir/lspsaga.nvim")
-    use("simrat39/symbols-outline.nvim")
+    use "glepnir/lspsaga.nvim"
+    use "simrat39/symbols-outline.nvim"
 
 
     -- Language Plugins
-    use("simrat39/rust-tools.nvim")
-
-
+    use "simrat39/rust-tools.nvim"
 end)
 
