9d8dda89faffa522b1a6f2116759441c4de75a7f
GeekMasher <geekmasher@gmail.com>
2022-10-17 17:16:54 +0100

    Remove require

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,6 @@ require('geekmasher.base')
 require('geekmasher.plugins')
 require('geekmasher.theme')
 require('geekmasher.mappings')
-require('geekmasher.staticfanatic')
 
 -- If Linux
 -- If MacOS
