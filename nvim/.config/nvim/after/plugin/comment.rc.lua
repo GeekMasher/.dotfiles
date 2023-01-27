5abf9f7918e3a0135863724909cec72e25b6adc0
GeekMasher <geekmasher@gmail.com>
2023-01-27 21:46:50 +0000

    Add <CTRL>/ for commenting

--- nvim/.config/nvim/after/plugin/comment.rc.lua
+++ nvim/.config/nvim/after/plugin/comment.rc.lua
@@ -1,6 +1,10 @@
 local status, comment = pcall(require, "Comment")
-if (not status) then return end 
+if (not status) then return end
 
-comment.setup {}
+comment.setup {
+    -- <C-_> == <CTRL> /
+    toggler = { line = '<C-_>' },
+    opleader = { line = '<C-_>' }
+}
 
 
