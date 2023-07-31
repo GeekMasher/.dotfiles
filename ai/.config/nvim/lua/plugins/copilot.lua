9febe52b92e37276eec61bc4d279210c37ecf8ec
GeekMasher <geekmasher@gmail.com>
2023-07-31 20:42:29 +0100

    fix: copilot and update deps

--- ai/.config/nvim/lua/plugins/copilot.lua
+++ ai/.config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,23 @@
 -- https://github.com/github/copilot.vim
+-- https://github.com/zbirenbaum/copilot.lua
 
 return {
-    "github/copilot.vim"
+    "zbirenbaum/copilot.lua",
+    event = "VeryLazy",
+    dependencies = {
+        "zbirenbaum/copilot-cmp"
+    },
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+            },
+            panel = {
+                enabled = false
+            },
+        })
+
+        require("copilot_cmp").setup()
+    end,
 }
