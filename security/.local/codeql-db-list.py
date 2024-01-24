6046de15a30f5393cf1a2316a6cce11f1b701392
GeekMasher <geekmasher@gmail.com>
2024-01-24 10:11:43 +0000

    feat: Update DB Script output

--- security/.local/codeql-db-list.py
+++ security/.local/codeql-db-list.py
@@ -2,7 +2,8 @@ import os
 from ghastoolkit import CodeQLDatabases
 
 local_databases = CodeQLDatabases()
-local_databases.findDatabases(os.environ.get("CODEQL_DATABASES"))
+if loc := os.environ.get("CODEQL_DATABASES"):
+    local_databases.findDatabases(loc)
 
 for database in local_databases:
-    print(f"{database.path}")
+    print(f"{database.name}\t{database.language}\t{database.created}\t{database.path}")
