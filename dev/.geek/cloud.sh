f08a3381044f8793d252606d323abad9320e347c
GeekMasher <2772944+GeekMasher@users.noreply.github.com>
2026-02-15 14:45:30 +0000

    feat: Move around scripts and configs

--- dev/.geek/cloud.sh
+++ dev/.geek/cloud.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
+# Azure
 # https://docs.microsoft.com/en-us/cli/azure/run-azure-cli-docker
-DOCKER_AZURE_IMAGE="mcr.microsoft.com/azure-cli:latest"
+export DOCKER_AZURE_IMAGE="mcr.microsoft.com/azure-cli:latest"
+
 alias azure="docker run --env-file=${HOME}/.env -v ${HOME}/.azure:/root/.azure -v ${HOME}/.ssh:/root/.ssh -it ${DOCKER_AZURE_IMAGE} az $@"
 alias azure-update="docker pull ${DOCKER_AZURE_IMAGE}"
 alias az="azure"
--- dev/.geek/git.sh
+++ dev/.geek/git.sh
@@ -9,17 +9,5 @@ alias clone-lite="git clone --recursive --depth 1 $@"
 alias pull="git pull"
 alias push="git push"
 
-# Change branch or create new branch
-function branch() {
-    if git rev-parse --verify --quiet "refs/heads/$1"; then
-        git checkout "$1"
-        git pull origin "$1"
-    else
-        git checkout -b "$1"
-    fi
-    git push --set-upstream origin "$1"
-}
-
 alias main="branch main"
 alias develop="branch develop"
-
