645c53661af3209e5d7b7778bb277f0dba00b20c
GeekMasher <mathew@xbow.com>
2026-03-26 09:45:25 +0000

    feat: Improvements to Ghostty

--- ghostty/.geek/ghostty.sh
+++ ghostty/.geek/ghostty.sh
@@ -2,5 +2,7 @@
 
 # Ghostty shell integration for Bash. This should be at the top of your bashrc!
 if [ -n "${GHOSTTY_RESOURCES_DIR}" ]; then
-    builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty.bash"
+    if [ -f "${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty.bash" ]; then
+        builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty.bash"
+    fi
 fi
